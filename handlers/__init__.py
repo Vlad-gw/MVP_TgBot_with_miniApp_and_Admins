5a7326d523fb84154d7711390152ed8cd0c71bce
Vladislav Gavrilov <gavrilovvlad.2004@mail.ru>
2026-03-31 22:28:41 +0300

    update

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,33 +1,23 @@
-# handlers/__init__.py
-
 from . import admin
 from . import start
 from . import profile
-from . import delete
 from . import balance
 from . import history
-from . import analytics
 from . import export
-from . import site_login
 from . import transactions
-from . import forecast
-from . import budget
 from . import quick_add
 from . import import_statement
+from . import reminders
 
 __all__ = [
     "admin",
     "start",
     "profile",
-    "delete",
     "balance",
     "history",
-    "analytics",
     "export",
-    "site_login",
     "transactions",
-    "forecast",
-    "budget",
     "quick_add",
     "import_statement",
+    "reminders",
 ]
